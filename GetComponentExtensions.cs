2d06e2dc03e2afac0af6a4ce079f3e48c7b42a64
Timo Naskali <45103521+timo-sisus@users.noreply.github.com>
2024-06-02 13:41:20 +0300

    Create GetComponentExtensions.cs

---
+++ GetComponentExtensions.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sisus.Init.Internal
+{
+	/// <summary>
+	/// Extensions methods for <see cref="GameObject"/> that can be used to get components without allocating any garbage.
+	/// </summary>
+	internal static class GetComponentExtensions
+	{
+		/// <summary>
+		/// Gets references to all components of type <typeparamref name="TComponent"/> on the <paramref name="gameObject"/>,
+		/// without allocating any garbage.
+		/// <para>
+		/// The result of this method should never be cached or reused; it should either be used with a using statement,
+		/// so that it gets disposed when leaving the method scope, or iterated once with a foreach statement,
+		/// in which case it gets disposed automatically at the end of the iteration.
+		/// </para>
+		/// </summary>
+		/// <typeparam name="TComponent"> The type of elements in the list; either a <see cref="Component"/> or interface type. </typeparam>
+		/// <param name="gameObject"> The game object to search. </param>
+		/// <returns> A list containing all matching components of type <typeparamref name="TComponent"/>. </returns>
+		/// <example>
+		/// <code>
+		/// foreach(var component in gameObject.GetComponentsNonAlloc{Component}())
+		/// {
+		///		// do something with component
+		/// }
+		/// </code>
+		/// </example>
+		/// <example>
+		/// <code>
+		/// using var components = gameObject.GetComponentsNonAlloc{Component}();
+		/// // do something with components
+		/// </code>
+		/// </example>
+		public static ComponentCollection<TComponent> GetComponentsNonAlloc<TComponent>(this GameObject gameObject)
+		{
+			ComponentCollection<TComponent> collection;
+
+			var cachedCollections = Cached<TComponent>.collections;
+			int count = cachedCollections.Count;
+			if(count > 0)
+			{
+				int lastIndex = count - 1;
+				collection = cachedCollections[lastIndex];
+				cachedCollections.RemoveAt(lastIndex);
+			}
+			else
+			{
+				collection = new ComponentCollection<TComponent>(1);
+			}
+
+			gameObject.GetComponents(collection);
+			return collection;
+		}
+
+		internal static class Cached<TComponent>
+		{
+			public static readonly List<ComponentCollection<TComponent>> collections = new();
+		}
+	}
+}
